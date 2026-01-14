02175301c866e6ea4f4230ad1417aa8aad6c0bae
Alzower <glenn.guillard.pros@gmail.com>
2026-01-14 11:49:15 +0100

    fix limite

--- back/middleware/rateLimiter.ts
+++ back/middleware/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 3,
+  max: 5,
   standardHeaders: true,
   legacyHeaders: false,
   keyGenerator: (req) => {
